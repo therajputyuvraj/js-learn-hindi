502f3f94f6889f8c96aced9c3e65e1b5fa2b1456
therajputyuvraj <yuvrajsrdata@gmail.com>
2025-05-17 20:06:56 +0530

    date and time and numbers

---
+++ 01_basics/dateAndTimes.js
@@ -0,0 +1,19 @@
+// //Date and Time
+
+// let myDate = new Date(); //made an object of date
+// console.log(myDate); // current date and time
+
+// console.log(myDate.toString()); // string representation of date
+
+// console.log(myDate.toLocaleString()); // locale string representation of date
+
+// let myCreatedDate = new Date('2025-01-01'); // created date
+
+// console.log(myCreatedDate.toString()); // created date
+
+//time Stamp
+
+let TimeStampt = Date.now(); //current time stamp
+
+console.log(TimeStampt.toFixed); // current time stamp
+
---
+++ 01_basics/numberAndMaths.js
@@ -0,0 +1,9 @@
+console.log("Number and Maths") 
+
+const score = 100.356728
+
+console.log(score.toFixed(2)) // 100.36;
+
+//Math
+
+
